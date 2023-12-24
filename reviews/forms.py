0068a0e2235b1b3cb74d84d8aad371f8a9560ccc
unknown <karimsalahuddin03@gmaqil.com>
2023-12-24 12:05:19 +0600

    use modelview

---
+++ reviews/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Review
+# class ReviewForm(forms.Form):
+#     # user_name= forms.CharField( label="Your Name", max_length=100, error_messages={
+#     #                                 "required":"your name must not be empty",
+#     #                                "max_length":"please enter a shorter name"
+                                   
+#     #                            }) 
+    
+#     # review_text=forms.CharField(label="your feedback", widget=forms.Textarea, max_length=200)
+#     # rating = forms.IntegerField(label="Your Rating", min_value=1,max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model=Review
+        fields='__all__'
+        labels ={
+            "user_name":"Your Name",
+            "review_name":"Your Feedback",
+            "rating":"Your rating"
+        }
+        error_names={
+            "user_name":{
+            "required":"your name must not be empty",
+            "max_length":"please enter a shorter name"
+                } 
+            }
